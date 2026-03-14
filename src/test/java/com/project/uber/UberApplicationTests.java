a1af22cc45dbcdeacf79114648aaf922123a8555
avanish-123 <avanishpandey022@gmail.com>
2026-03-14 23:02:07 +0530

    email support added and application.properties file removed from tracking

--- src/test/java/com/project/uber/UberApplicationTests.java
+++ src/test/java/com/project/uber/UberApplicationTests.java
@@ -1,13 +1,18 @@
 package com.project.uber;
 
+import com.project.uber.services.EmailSenderService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UberApplicationTests {
+	@Autowired
+	private EmailSenderService emailSenderService;
 
 	@Test
 	void contextLoads() {
+		emailSenderService.sendEmail(new String[]{"avanishchandrapandey022@gmail.com", "hexagon.jpeg@gmail.com"}, "test email", "test email body");
 	}
 
 }
